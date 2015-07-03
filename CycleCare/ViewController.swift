61fa388f67dc7b5f8d71d4434f30811890dd7b26
CGS Spare <spare@toms-air-3.grammar.local>
2015-07-03 14:40:03 +1000

    Initial Commit

---
+++ CycleCare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CycleCare
+//
+//  Created by CGS Spare on 3/07/2015.
+//  Copyright (c) 2015 tidbit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CycleCareTests/CycleCareTests.swift
@@ -0,0 +1,36 @@
+//
+//  CycleCareTests.swift
+//  CycleCareTests
+//
+//  Created by CGS Spare on 3/07/2015.
+//  Copyright (c) 2015 tidbit. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CycleCareTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
